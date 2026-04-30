a71d37d1d29057d2414df08aa8f3801604585740
DavidElPapu <david.jung@uniat.edu.mx>
2026-04-30 02:05:08 -0600

    Input manager singleton creado y el player ya puede correr

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine.Windows;
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerInputActions inputActions;
-    [SerializeField] private float speedLimiter, walkSpeed, runSpeed, rotationSpeed;
+    [SerializeField] private float walkSpeed, runSpeed, rotationSpeed;
     private Rigidbody rb;
 
     private float speed;
@@ -13,9 +13,22 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         TryGetComponent(out rb);
-        inputActions = new PlayerInputActions();
-        inputActions.Player.Enable();
+    }
+
+    private void OnEnable()
+    {
+        //Commented 2 lines below this because the input creation was moved to a singleton in InputManager script
+        //inputActions = new PlayerInputActions();
+        //inputActions.Player.Enable();
+        inputActions = InputManager.singleton.inputActions;
         inputActions.Player.Run.performed += OnRunInput;
+        inputActions.Player.Run.canceled += OnRunInput;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Run.performed -= OnRunInput;
+        inputActions.Player.Run.canceled -= OnRunInput;
     }
 
     private void Start()
@@ -31,24 +44,31 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         MovePlayer();
-        RotatePlayer();
     }
 
     private void MovePlayer()
     {
-        Vector2 moveInput = inputActions.Player.Move.ReadValue<Vector2>();
-        Vector3 movementVector = new Vector3(moveInput.x, 0, moveInput.y).normalized;
+        Vector2 moveInput = InputManager.singleton.inputActions.Player.Move.ReadValue<Vector2>();
+        Vector3 movementVector = new Vector3(moveInput.x, 0, moveInput.y);
 
         rb.AddForce(movementVector * speed, ForceMode.Force);
-    }
 
-    private void RotatePlayer()
-    {
-        
+        //Rotates the player towards movement direction
+        if (movementVector != Vector3.zero) 
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementVector);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.fixedDeltaTime);
+            //Below are alternatives to rotation that also worked, just in case
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.fixedDeltaTime);
+            //rb.MoveRotation(toRotation);
+        }
     }
 
     private void OnRunInput(InputAction.CallbackContext context)
     {
-        print(context.phase);
+        if (context.phase == InputActionPhase.Performed)
+            speed = runSpeed;
+        else
+            speed = walkSpeed;
     }
 }
