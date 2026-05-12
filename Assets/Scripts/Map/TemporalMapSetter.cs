cd0efaa0170eb8682ef76d7802f78b847c86f210
AMiranda444 <america.esparza@uniat.edu.mx>
2026-05-12 15:18:11 -0600

    Contenedor objeto creado para test

--- Assets/Scripts/Map/TemporalMapSetter.cs
+++ Assets/Scripts/Map/TemporalMapSetter.cs
@@ -4,12 +4,24 @@ using System.Collections.Generic;
 public class TemporalMapSetter : MonoBehaviour
 {
     public List<GameObject> stationsOnMap;
+    public List<GameObject> containersOnMap;
 
     private void Start()
     {
         foreach (GameObject station in stationsOnMap)
         {
             MapGridManager.singleton.AddObjectAt(station.transform.position, station);
+        }
+        foreach (GameObject container in containersOnMap)
+        {
+            GameObject possibleStation = MapGridManager.singleton.GetGameObjectAt(container.transform.position);
+            if (possibleStation != null && possibleStation.TryGetComponent(out IPlaceable stationInteraction))
+            {
+                if (stationInteraction.CanPlaceItem(container))
+                {
+                    stationInteraction.CanPlaceItem(container);
+                }
+            }
         }
     }
 }
