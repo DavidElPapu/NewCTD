c504079ef6144ec674c10f8da2a60bfb9c5857c4
DavidElPapu <david.jung@uniat.edu.mx>
2026-05-25 11:58:27 -0600

    Tower folder created and base Tower empty

--- Assets/Scripts/Recipes/RecipesManager.cs
+++ Assets/Scripts/Recipes/RecipesManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class RecipesManager : MonoBehaviour
 {
     public static RecipesManager singleton;
-    [SerializeField] private List<RecipeSO> allRecipesSO = new List<RecipeSO>();
+    [SerializeField] private List<RecipeSO> allRecipesSO;
     private HashSet<RecipeScript> allRecipes, levelRecipes;
 
     private void Awake()
---
+++ Assets/Scripts/Towers/BaseTower.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BaseTower : MonoBehaviour
+{
+    //Reference to the SO
+    [SerializeField] private List<GameObject> levelModels;
+
+}
