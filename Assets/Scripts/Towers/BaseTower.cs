89da558a8d502f16ecd086f109a19024bfcbaa50
DavidElPapu <david.jung@uniat.edu.mx>
2026-05-25 16:48:54 -0600

    BaseTower script modified

--- Assets/Scripts/Towers/BaseTower.cs
+++ Assets/Scripts/Towers/BaseTower.cs
@@ -1,9 +1,38 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class BaseTower : MonoBehaviour
+public abstract class BaseTower : MonoBehaviour
 {
     //Reference to the SO
+    //DetectionRange
     [SerializeField] private List<GameObject> levelModels;
+    protected List<GameObject> enemiesInRange = new List<GameObject>();
+    private int currentLevel = 0;
+
+    protected virtual void Awake()
+    {
+        for (int i = 0; i < levelModels.Count; i++)
+        {
+            if (i != currentLevel)
+                levelModels[i].SetActive(false);
+            else
+                levelModels[i].SetActive(true);
+        }
+    }
+
+    public virtual bool CanUpgrade()
+    {
+        if (currentLevel < levelModels.Count - 1) return true;
+        return false;
+    }
+
+    public virtual void OnUpgrade()
+    {
+        levelModels[currentLevel].SetActive(false);
+        currentLevel++;
+        levelModels[currentLevel].SetActive(true);
+    }
+
+    protected abstract void OnMainAction();
 
 }
